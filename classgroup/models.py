c6f4a253644a094300ea1856aab264098c65c877
knight <wanderknight@gmail.com>
2019-08-13 18:05:49 +0800

    user classgroup 关系。

--- classgroup/models.py
+++ classgroup/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from account.models import User
 
 
 class Classgroup(models.Model):
     title = models.TextField()
+    classgroups = models.ManyToManyField(User)
 
     class Meta:
         db_table = "classgroup"
